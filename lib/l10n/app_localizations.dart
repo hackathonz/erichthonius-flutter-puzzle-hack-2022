16fec0f28ff4c2e40c30e6ed54692d72ddafd6ae
freitzzz <1160907@isep.ipp.pt>
2022-02-23 23:40:02 +0000

    Import every known localization string

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -37,6 +37,280 @@ class AppLocalizations {
       locale: localeName,
     );
   }
+
+  String get play {
+    return Intl.message(
+      'Play',
+      name: 'play',
+      desc: 'Play keyword',
+      locale: localeName,
+    );
+  }
+
+  String get ccontinue {
+    return Intl.message(
+      'Continue',
+      name: 'ccontinue',
+      desc: 'Continue keyword',
+      locale: localeName,
+    );
+  }
+
+  String get easy {
+    return Intl.message(
+      'Easy',
+      name: 'easy',
+      desc: 'Easy keyword',
+      locale: localeName,
+    );
+  }
+
+  String get medium {
+    return Intl.message(
+      'Medium',
+      name: 'medium',
+      desc: 'Medium keyword',
+      locale: localeName,
+    );
+  }
+
+  String get hard {
+    return Intl.message(
+      'Hard',
+      name: 'hard',
+      desc: 'Hard keyword',
+      locale: localeName,
+    );
+  }
+
+  String get pieces {
+    return Intl.message(
+      'Pieces',
+      name: 'pieces',
+      desc: 'Pieces keyword',
+      locale: localeName,
+    );
+  }
+
+  String get shuffle {
+    return Intl.message(
+      'Shuffle',
+      name: 'shuffle',
+      desc: 'Shuffle keyword',
+      locale: localeName,
+    );
+  }
+
+  String get retry {
+    return Intl.message(
+      'Retry',
+      name: 'retry',
+      desc: 'Retry keyword',
+      locale: localeName,
+    );
+  }
+
+  String get puzzleCompletedDialogTitle {
+    return Intl.message(
+      'YOU MADE IT 👏',
+      name: 'puzzleCompletedDialogTitle',
+      desc: 'Title of the dialog that is shown after the puzzle is completed',
+      locale: localeName,
+    );
+  }
+
+  String get nextLevel {
+    return Intl.message(
+      'Next Level',
+      name: 'nextLevel',
+      desc: 'Next Level keyword',
+      locale: localeName,
+    );
+  }
+
+  String get exitGame {
+    return Intl.message(
+      'Exit Game',
+      name: 'exitGame',
+      desc: 'Exit game keyword',
+      locale: localeName,
+    );
+  }
+
+  String get timeCaps {
+    return Intl.message(
+      'TIME',
+      name: 'timeCaps',
+      desc: 'Time keyword in all capital case',
+      locale: localeName,
+    );
+  }
+
+  String welcomeUser(final String username) {
+    return Intl.message(
+      'Hi $username 👋,',
+      name: 'welcomeUser',
+      desc: 'Message to welcome user',
+      args: [username],
+      locale: localeName,
+    );
+  }
+
+  String get chooseDifficulty {
+    return Intl.message(
+      'Choose a difficulty',
+      name: 'chooseDifficulty',
+      desc: 'Message that prompts user to select the games difficulty',
+      locale: localeName,
+    );
+  }
+
+  String get leaderboardTitle {
+    return Intl.message(
+      'Leaderboard',
+      name: 'leaderboardTitle',
+      desc: 'Title of leaderboard page',
+      locale: localeName,
+    );
+  }
+
+  String totalPoints(final int points) {
+    return Intl.message(
+      '$points pts',
+      name: 'totalPoints',
+      desc: 'Describes how many points an user has',
+      args: [points],
+      locale: localeName,
+    );
+  }
+
+  String get today {
+    return Intl.message(
+      'Today',
+      name: 'today',
+      desc: 'Today keyword',
+      locale: localeName,
+    );
+  }
+
+  String get week {
+    return Intl.message(
+      'Week',
+      name: 'week',
+      desc: 'Week keyword',
+      locale: localeName,
+    );
+  }
+
+  String get month {
+    return Intl.message(
+      'Month',
+      name: 'month',
+      desc: 'Month keyword',
+      locale: localeName,
+    );
+  }
+
+  String get settingsTitle {
+    return Intl.message(
+      'Settings',
+      name: 'settingsTitle',
+      desc: 'Title of settings page',
+      locale: localeName,
+    );
+  }
+
+  String get changeProfileDetailsOption {
+    return Intl.message(
+      'Change profile details',
+      name: 'changeProfileDetailsOption',
+      desc: 'Settings page option to change user profile details',
+      locale: localeName,
+    );
+  }
+
+  String get notificationsOption {
+    return Intl.message(
+      'Notifications',
+      name: 'notificationsOption',
+      desc: 'Settings page option to enable/disable notifications',
+      locale: localeName,
+    );
+  }
+
+  String get soundsOption {
+    return Intl.message(
+      'Sounds',
+      name: 'soundsOption',
+      desc: 'Settings page option to enable/disable sounds',
+      locale: localeName,
+    );
+  }
+
+  String get vibrationOption {
+    return Intl.message(
+      'Vibration',
+      name: 'vibrationOption',
+      desc: 'Settings page option to enable/disable phone vibration',
+      locale: localeName,
+    );
+  }
+
+  String get profileDetailsTitle {
+    return Intl.message(
+      'Profile Details',
+      name: 'profileDetailsTitle',
+      desc: 'Title of user profile details page',
+      locale: localeName,
+    );
+  }
+
+  String puzzleGameLevel(final String level) {
+    return Intl.message(
+      'Lv. $level',
+      name: 'puzzleGameLevel',
+      desc:
+          'Indicator of the puzzle game level the user is currently or about to play',
+      args: [level],
+      locale: localeName,
+    );
+  }
+
+  String puzzleGameTimeLeft(final int secondsLeft) {
+    final mins = (secondsLeft / 60).floor();
+
+    final secs = secondsLeft - (mins * 60);
+
+    final useMins = mins > 0 ? 1 : 0;
+
+    return _puzzleGameTimeLeft(useMins, mins, secs);
+  }
+
+  String _puzzleGameTimeLeft(
+    final int useMins,
+    final int mins,
+    final int secs,
+  ) {
+    return Intl.plural(
+      useMins,
+      zero: '$secs s',
+      other: '$mins m $secs s',
+      name: '_puzzleGameTimeLeft',
+      desc: 'Indicator of the time left to complete the puzzle game',
+      args: [useMins, mins, secs],
+      locale: localeName,
+    );
+  }
+
+  String puzzleGameTimeFinalScore(final int score) {
+    return Intl.message(
+      'Score: $score pts',
+      name: 'puzzleGameTimeFinalScore',
+      desc: 'Indicator of the score achieved by completing the puzzle game',
+      args: [score],
+      locale: localeName,
+    );
+  }
 }
 
 class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
--- lib/l10n/messages/messages_messages.dart
+++ lib/l10n/messages/messages_messages.dart
@@ -19,8 +19,46 @@ typedef String MessageIfAbsent(String? messageStr, List<Object>? args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'messages';
 
+  static m0(useMins, mins, secs) => "${Intl.plural(useMins, zero: '${secs} s', other: '${mins} m ${secs} s')}";
+
+  static m1(level) => "Lv. ${level}";
+
+  static m2(score) => "Score: ${score} pts";
+
+  static m3(points) => "${points} pts";
+
+  static m4(username) => "Hi ${username} 👋,";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function> {
-    "title" : MessageLookupByLibrary.simpleMessage("Hello World")
+    "_puzzleGameTimeLeft" : m0,
+    "ccontinue" : MessageLookupByLibrary.simpleMessage("Continue"),
+    "changeProfileDetailsOption" : MessageLookupByLibrary.simpleMessage("Change profile details"),
+    "chooseDifficulty" : MessageLookupByLibrary.simpleMessage("Choose a difficulty"),
+    "easy" : MessageLookupByLibrary.simpleMessage("Easy"),
+    "exitGame" : MessageLookupByLibrary.simpleMessage("Exit Game"),
+    "hard" : MessageLookupByLibrary.simpleMessage("Hard"),
+    "leaderboardTitle" : MessageLookupByLibrary.simpleMessage("Leaderboard"),
+    "medium" : MessageLookupByLibrary.simpleMessage("Medium"),
+    "month" : MessageLookupByLibrary.simpleMessage("Month"),
+    "nextLevel" : MessageLookupByLibrary.simpleMessage("Next Level"),
+    "notificationsOption" : MessageLookupByLibrary.simpleMessage("Notifications"),
+    "pieces" : MessageLookupByLibrary.simpleMessage("Pieces"),
+    "play" : MessageLookupByLibrary.simpleMessage("Play"),
+    "profileDetailsTitle" : MessageLookupByLibrary.simpleMessage("Profile Details"),
+    "puzzleCompletedDialogTitle" : MessageLookupByLibrary.simpleMessage("YOU MADE IT 👏"),
+    "puzzleGameLevel" : m1,
+    "puzzleGameTimeFinalScore" : m2,
+    "retry" : MessageLookupByLibrary.simpleMessage("Retry"),
+    "settingsTitle" : MessageLookupByLibrary.simpleMessage("Settings"),
+    "shuffle" : MessageLookupByLibrary.simpleMessage("Shuffle"),
+    "soundsOption" : MessageLookupByLibrary.simpleMessage("Sounds"),
+    "timeCaps" : MessageLookupByLibrary.simpleMessage("TIME"),
+    "title" : MessageLookupByLibrary.simpleMessage("Hello World"),
+    "today" : MessageLookupByLibrary.simpleMessage("Today"),
+    "totalPoints" : m3,
+    "vibrationOption" : MessageLookupByLibrary.simpleMessage("Vibration"),
+    "week" : MessageLookupByLibrary.simpleMessage("Week"),
+    "welcomeUser" : m4
   };
 }
