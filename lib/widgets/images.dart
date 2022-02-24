17436d641909845506f391eca2243e990d87a2d3
freitzzz <1160907@isep.ipp.pt>
2022-02-24 00:47:23 +0000

    Defined swap it alternative logo

--- lib/widgets/images.dart
+++ lib/widgets/images.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 const _kSwapItLogoAssetPath = 'assets/images/swap_it_logo.png';
+const _kSwapItAlternativeLogoAssetPath =
+    'assets/images/swap_it_alternative_logo.png';
 
 class SwapItLogo extends StatelessWidget {
   const SwapItLogo({
@@ -11,6 +13,19 @@ class SwapItLogo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Image.asset(
       _kSwapItLogoAssetPath,
+    );
+  }
+}
+
+class SwapItAlternativeLogo extends StatelessWidget {
+  const SwapItAlternativeLogo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      _kSwapItAlternativeLogoAssetPath,
     );
   }
 }
