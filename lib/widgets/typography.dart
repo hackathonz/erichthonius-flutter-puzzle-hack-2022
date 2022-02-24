24a9d0773e2184ce5a8e0dc1876e60dcfd098009
freitzzz <1160907@isep.ipp.pt>
2022-02-24 13:18:06 +0000

    Define typography for buttons

---
+++ lib/widgets/typography.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:swap_it/widgets/widgets.dart';
+
+final appTextTheme = GoogleFonts.poppinsTextTheme();
+
+const primaryButtonTextStyle = TextStyle(
+  color: primaryTextColor,
+  fontSize: 16,
+  letterSpacing: 0,
+  fontWeight: FontWeight.w600,
+  height: 1,
+);
