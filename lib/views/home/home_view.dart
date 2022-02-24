f1e57dc185db2a7a4eca8d8ded92ee868714b350
freitzzz <1160907@isep.ipp.pt>
2022-02-24 20:18:01 +0000

    Build only play button if user has not played any game yet

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:swap_it/blocs/blocs.dart';
 import 'package:swap_it/l10n/app_localizations.dart';
 import 'package:swap_it/widgets/widgets.dart';
 
@@ -11,55 +12,74 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context);
 
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 64.0),
-        child: ListView(
-          children: [
-            const SwapItAlternativeLogo(),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 44.0),
-            ),
-            SwapItButton(
-              text: localizations.play,
-              onPressed: () {},
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 12.0),
-            ),
-            SwapItButton(
-              text: localizations.ccontinue,
-              onPressed: () {},
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 44.0),
-            ),
-            Row(
-              children: [
-                IconBase(
-                  child: const Icon(
-                    SwapItIcons.profile,
-                  ),
-                  onPressed: () {},
+    final gameBloc = context.read<GameBloc>();
+
+    final showContinueButton = gameBloc.game.gameUserProfile.hasPlayedAnyGame;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 64.0),
+      child: Scaffold(
+        body: Center(
+          child: ListView(
+            children: [
+              const SwapItAlternativeLogo(),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 44.0 + (!showContinueButton ? 12.0 : 0.0),
                 ),
-                IconBase.alternative(
-                  child: const Icon(
-                    SwapItIcons.cup,
-                    size: kIconAlternativeSize,
+              ),
+              SwapItButton(
+                text: localizations.play,
+                onPressed: () {},
+              ),
+              if (showContinueButton)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  child: SwapItButton(
+                    text: localizations.ccontinue,
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
                 ),
-                IconBase(
-                  child: const Icon(
-                    SwapItIcons.settings,
-                  ),
-                  onPressed: () {},
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 44.0),
+              ),
+            ],
+            primary: false,
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedFontSize: 32.0,
+          unselectedFontSize: 32.0,
+          items: [
+            BottomNavigationBarItem(
+              label: localizations.profileDetailsTitle,
+              icon: IconBase(
+                child: const Icon(
+                  SwapItIcons.profile,
                 ),
-              ],
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                onPressed: () {},
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: localizations.play,
+              icon: IconBase.alternative(
+                child: const Icon(
+                  SwapItIcons.cup,
+                  size: kIconAlternativeSize,
+                ),
+                onPressed: () {},
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: localizations.settingsTitle,
+              icon: IconBase(
+                child: const Icon(
+                  SwapItIcons.settings,
+                ),
+                onPressed: () {},
+              ),
             ),
           ],
-          primary: false,
         ),
       ),
     );
