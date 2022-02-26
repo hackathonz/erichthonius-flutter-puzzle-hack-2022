0e918fa641a8a4cee6e72848161528c69c5f601f
freitzzz <1160907@isep.ipp.pt>
2022-02-26 22:10:13 +0000

    Settings view ready

--- lib/views/settings/settings_view.dart
+++ lib/views/settings/settings_view.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:swap_it/blocs/blocs.dart';
 import 'package:swap_it/l10n/app_localizations.dart';
+import 'package:swap_it/models/models.dart';
 import 'package:swap_it/widgets/widgets.dart';
 
-const _kAppbarTitleInBetweenPadding = EdgeInsets.symmetric(
-  vertical: 5.0,
-);
-
 const _kListViewPadding = EdgeInsets.symmetric(
-  vertical: 16.0,
+  vertical: 24.0,
 );
 
 const _kSeparatorPadding = EdgeInsets.symmetric(
@@ -24,11 +21,58 @@ class SettingsView extends StatelessWidget {
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context);
 
+    final settingsBloc = context.read<SettingsBloc>();
+
+    final settingOptions = [
+      MapEntry(
+        SettingOption.notifications,
+        localizations.notificationsOption,
+      ),
+      MapEntry(
+        SettingOption.sounds,
+        localizations.soundsOption,
+      ),
+      MapEntry(
+        SettingOption.vibration,
+        localizations.vibrationOption,
+      ),
+    ];
+
     return SwapItScaffold(
       appBar: SwapItAppBar(
         title: localizations.settingsTitle,
       ),
-      body: ListView(),
+      body: BlocBuilder<SettingsBloc, SettingsState>(
+        buildWhen: (previous, current) =>
+            current is RetrieveSettingOptionsValuesSuccess,
+        builder: (context, state) {
+          final optionValues = state is RetrieveSettingOptionsValuesSuccess
+              ? state.optionValues
+              : settingsBloc.optionValues;
+
+          return ListView.separated(
+            itemBuilder: (context, index) => SwapItSwitchListTile(
+              leading: settingOptions[index].value,
+              switchInitialValue:
+                  optionValues[settingOptions[index].key] ?? false,
+              onSwitchValueChange: (value) {
+                settingsBloc.add(
+                  SettingOptionValueUpdated(
+                    option: settingOptions[index].key,
+                    value: value,
+                  ),
+                );
+              },
+            ),
+            separatorBuilder: (context, index) => const Padding(
+              padding: _kSeparatorPadding,
+            ),
+            itemCount: settingOptions.length,
+            padding: _kListViewPadding,
+            primary: false,
+          );
+        },
+      ),
     );
   }
 }
