d9937bf00d4173716303d32aac12501eb896f9a8
freitzzz <1160907@isep.ipp.pt>
2022-03-01 12:48:47 +0000

    Puzzle game widgets

---
+++ lib/widgets/puzzle.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:swap_it/models/models.dart';
+import 'package:swap_it/widgets/widgets.dart';
+
+class PuzzleGameTile extends StatelessWidget {
+  final PictureTile tile;
+
+  final Size tileSize;
+
+  const PuzzleGameTile({
+    Key? key,
+    required final this.tile,
+    required final this.tileSize,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      key: Key('puzzle_game_tile_${tile.id}'),
+      borderRadius: const BorderRadius.all(
+        Radius.circular(kPictureTileRadius),
+      ),
+      child: Image(
+        image: tile.imageProvider,
+        fit: BoxFit.cover,
+        width: tileSize.width,
+        height: tileSize.height,
+      ),
+    );
+  }
+}
+
+class PuzzleGame extends StatelessWidget {
+  final LevelDifficulty difficulty;
+
+  final List<PictureTile> tiles;
+
+  const PuzzleGame({
+    Key? key,
+    required final this.tiles,
+    required final this.difficulty,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final tileSize = mapPictureTileSizeForDifficulty(difficulty);
+
+    final tileCount = mapPuzzleGameTileCountForDifficulty(difficulty);
+
+    final crossAxisCount = sqrt(tileCount).floor();
+
+    return GridView.count(
+      crossAxisCount: crossAxisCount,
+      children: [
+        ...tiles.map(
+          (x) => PuzzleGameTile(
+            tile: x,
+            tileSize: tileSize,
+          ),
+        ),
+      ],
+      crossAxisSpacing: kPuzzleGameTilesSpacing,
+      mainAxisSpacing: kPuzzleGameTilesSpacing,
+      primary: false,
+    );
+  }
+}
