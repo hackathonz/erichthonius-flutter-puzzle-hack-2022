0f540c6737d6e23d111e847be04cd639924d6d9c
freitzzz <1160907@isep.ipp.pt>
2022-03-01 12:48:55 +0000

    Model for puzzle game picture tile

---
+++ lib/models/tile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PictureTile {
+  final int id;
+
+  final ImageProvider imageProvider;
+
+  const PictureTile({
+    required final this.id,
+    required final this.imageProvider,
+  });
+}
